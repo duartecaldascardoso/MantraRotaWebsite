886fd8965db5758559fe67a158fdf1134f5c5c0a
caldas33 <126091251+caldas33@users.noreply.github.com>
2024-03-28 17:36:15 +0000

    Revert "Adding the initial implementation to the website "
    This reverts commit b10ebbd6d73f7380f7c34fdfe6b1a41c0114f68a.

--- ReactApplication/mantraRota-frontend/src/main.jsx
+++ ReactApplication/mantraRota-frontend/src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './pages/Home.jsx'
+import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>,
 )
