d5c435b57cd6ed38f53ff2dd4b828e2768cca1ed
caldas33 <126091251+caldas33@users.noreply.github.com>
2024-03-30 19:07:15 +0000

    Optimizing imports

--- ReactApplication/mantraRota-frontend/src/App.jsx
+++ ReactApplication/mantraRota-frontend/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Home from './pages/Home'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, } from 'react-router-dom'
 
 function App() {
 
